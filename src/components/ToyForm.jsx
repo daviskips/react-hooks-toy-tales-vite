ba5426695f6b0661fea52edc8fd6314957aad01a
daviskiplangat <daviskiplangat74@gmail.com>
2026-08-17 15:40:51 +0300

    Fix ToyForm submit to read inputs reliably

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -4,16 +4,19 @@ function ToyForm({ onAddToy }) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    const form = event.target;
+    const form = event.currentTarget || (event.target && event.target.closest && event.target.closest("form"));
+
+    const nameInput = form.querySelector('input[name="name"]');
+    const imageInput = form.querySelector('input[name="image"]');
 
     const newToy = {
-      name: form.name.value,
-      image: form.image.value,
+      name: nameInput ? nameInput.value : "",
+      image: imageInput ? imageInput.value : "",
       likes: 0,
     };
 
     onAddToy(newToy);
-    form.reset();
+    if (form && form.reset) form.reset();
   }
 
   return (
